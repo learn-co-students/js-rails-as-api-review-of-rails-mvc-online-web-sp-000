34b5cc42f1b60a7f4d017f59df233247f2c304dd
Lauren McCoy <lm_lutec@outlook.com>
2020-03-24 17:31:32 -0400

    Done.

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -1,1 +1,11 @@
 # Add code from Readme
+class BirdsController < ApplicationController
+    def create
+        @bird = Bird.new 
+    end 
+
+    def index
+        @birds = Bird.all 
+        render 'birds/index.html.erb'
+    end 
+end 
